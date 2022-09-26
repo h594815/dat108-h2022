dec6acfe6e237119e61abe7b1ac5c0044541374c
h594815 <marius.helgas@hotmail.com>
2022-09-26 16:18:58 +0200

    k

--- F09 - Last ned til timen/src/no/hvl/dat108/f09/Eksempel1.java
+++ F09 - Last ned til timen/src/no/hvl/dat108/f09/Eksempel1.java
@@ -2,6 +2,7 @@ package no.hvl.dat108.f09;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Eksempel1 {
 	
@@ -15,8 +16,36 @@ public class Eksempel1 {
 				new Person("Matthew", "Arnold", 39));
 		
 		// Summen av aldre
+//		int sumAlder = people.stream()
+//				.map(p -> p.getAge())
+//				.reduce(0, (sum,element) -> sum+element);
+//		
+//		System.out.println(sumAlder);
+//		
+//		int sumAlder = people.stream()
+//					.map(p -> p.getAge())
+//					.reduce(0, (akk,e) -> Integer.sum(akk, e));
+//			
+//			System.out.println(sumAlder);
+//			
+//			int sumAlder = people.stream()
+//					.map(p -> p.getAge())
+//					.reduce(0, Integer::sum);
+//			
+//			System.out.println(sumAlder);
+		
+		int sumAlder = people.stream()
+				.mapToInt(Person::getAge)
+				.reduce(0, Integer::sum);
+		
+		System.out.println(sumAlder);
 		
 		// En streng med alle initialene, "CD LC TC CB MA"
+		
+		String initialer = people.stream().map(p -> " " + p.getFirstName().charAt(0) + p.getLastName().charAt(0)
+				).collect(Collectors.joining(" "));
+		
+		System.out.println("|" + initialer + "|");
 	}	
 
 }
